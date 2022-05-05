7e7b3e3c7a05c9bf7fc5308511426da90ad59f1c
mick1031 <qqq19871031@gmail.com>
2022-05-06 02:03:55 +0800

    modify app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var postsRouter = require('./routes/posts');
 var app = express();
 
 dotenv.config({ path: './config.env' });
-const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+var DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB)
     .then(() => {
         console.log('資料庫連線成功')
